0f9678d04e3f95e08a6743ed3351475eff291d5c
Amir Kabiri <akabiridev@gmail.com>
2025-11-06 17:00:58 +0330

    perf: refactor tsup.config.ts and minify bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,28 +2,40 @@ import { defineConfig } from 'tsup';
 import packageJson from './package.json';
 import { promises as fs } from 'fs';
 
+function copyFiles(files: string[]): Promise<void> {
+  return Promise.all(
+    files.map(file => fs.copyFile(file, `./dist/${file}`))
+  ).then(() => void 0);
+}
+
+function createFinalPackageJson() {
+  const newPackageJson = JSON.parse(JSON.stringify(packageJson));
+  delete newPackageJson.dependencies;
+  delete newPackageJson.devDependencies;
+  delete newPackageJson.scripts;
+  delete newPackageJson.files;
+  return {
+    ...newPackageJson,
+    main: './cli.cjs',
+    bin: {
+      chvm: './cli.cjs',
+    },
+  }
+}
+
 export default defineConfig({
   entry: ['src/cli.ts'],
   format: ['cjs'],
   shims: true,
   clean: true,
-  minify: false,
+  minify: true,
   noExternal: Object.keys(packageJson.dependencies),
   onSuccess: async () => {
-    let newPackageJson = JSON.parse(JSON.stringify(packageJson));
-    delete newPackageJson.dependencies;
-    delete newPackageJson.devDependencies;
-    delete newPackageJson.scripts;
-    delete newPackageJson.files;
-    newPackageJson = {
-      ...newPackageJson,
-      main: './cli.cjs',
-      bin: {
-        chvm: './cli.cjs',
-      },
-    }
-    await fs.writeFile('./dist/package.json', JSON.stringify(newPackageJson, null, 2));
-    await fs.copyFile('./README.md', './dist/README.md');
+    await fs.writeFile(
+      './dist/package.json',
+      JSON.stringify(createFinalPackageJson(), null, 2)
+    );
+    await copyFiles(['README.md', 'CHANGELOG.md']);
     console.log('Build successful');
   },
 });
