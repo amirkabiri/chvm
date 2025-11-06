a2cd96752b36390da4b621eb657a1906c2b0cd0a
Amir Kabiri <akabiridev@gmail.com>
2025-11-06 16:37:21 +0330

    fix: removing unnecessary fields of package when publishing to npm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
+import packageJson from './package.json';
+import { promises as fs } from 'fs';
 
 export default defineConfig({
   entry: ['src/cli.ts'],
@@ -6,5 +8,22 @@ export default defineConfig({
   shims: true,
   clean: true,
   minify: false,
-  noExternal: ['chalk', 'ora', 'cli-progress']
-})
+  noExternal: Object.keys(packageJson.dependencies),
+  onSuccess: async () => {
+    let newPackageJson = JSON.parse(JSON.stringify(packageJson));
+    delete newPackageJson.dependencies;
+    delete newPackageJson.devDependencies;
+    delete newPackageJson.scripts;
+    delete newPackageJson.files;
+    newPackageJson = {
+      ...newPackageJson,
+      main: './cli.cjs',
+      bin: {
+        chvm: './cli.cjs',
+      },
+    }
+    await fs.writeFile('./dist/package.json', JSON.stringify(newPackageJson, null, 2));
+    await fs.copyFile('./README.md', './dist/README.md');
+    console.log('Build successful');
+  },
+});
