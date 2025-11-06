ae64198a8e4a9681ba0339e3a228628a21368d96
Amir Kabiri <akabiridev@gmail.com>
2025-11-06 18:44:40 +0330

    fix: automatic version bump in package.json and dependency list optimization fixed

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,32 +1,4 @@
 import { defineConfig } from 'tsup';
-import packageJson from './package.json';
-import { promises as fs } from 'fs';
-
-function copyFiles(files: string[]): Promise<void> {
-  return Promise.all(
-    files.map(file => fs.copyFile(file, `./dist/${file}`))
-  ).then(() => void 0);
-}
-
-async function createFinalPackageJson() {
-  // Read the latest package.json to get the updated version
-  const currentPackageJson = JSON.parse(
-    await fs.readFile('./package.json', 'utf-8')
-  );
-  
-  const newPackageJson = JSON.parse(JSON.stringify(currentPackageJson));
-  delete newPackageJson.dependencies;
-  delete newPackageJson.devDependencies;
-  delete newPackageJson.scripts;
-  delete newPackageJson.files;
-  return {
-    ...newPackageJson,
-    main: './cli.cjs',
-    bin: {
-      chvm: './cli.cjs',
-    },
-  }
-}
 
 export default defineConfig({
   entry: ['src/cli.ts'],
@@ -34,13 +6,5 @@ export default defineConfig({
   shims: true,
   clean: true,
   minify: true,
-  noExternal: Object.keys(packageJson.dependencies),
-  onSuccess: async () => {
-    await fs.writeFile(
-      './dist/package.json',
-      JSON.stringify(await createFinalPackageJson(), null, 2)
-    );
-    await copyFiles(['README.md', 'CHANGELOG.md']);
-    console.log('Build successful');
-  },
+  noExternal: ['chalk', 'cli-progress', 'commander', 'ora', 'proper-lockfile'],
 });
